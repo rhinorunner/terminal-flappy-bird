737b6a7d58c1615b830a2099d4b8db782c08ff31
Zaine <74148219+rhinorunner@users.noreply.github.com>
2022-10-31 16:29:57 +0000

    penios

--- render.cpp
+++ render.cpp
@@ -1,1 +1,32 @@
+#ifndef RENDER_CPP_CHECK
+#define RENDER_CPP_CHECK
 
+#include <iostream>
+
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <unistd.h>
+#endif
+
+#ifdef _WIN32
+uint8_t getInput() {
+	while (true) {
+		uint8_t keyIn = (uint8_t) getch();
+		if (keyIn == 3) {exit(0);}
+		return keyIn;
+	}
+}
+#else
+uint8_t getInput() {
+	while (true) {
+		system("stty raw");
+        uint8_t keyIn = (uint8_t) getchar(); 
+        system("stty cooked");
+		if (keyIn == 3) {exit(0);}
+		return keyIn;
+	}
+}
+#endif
+
+#endif
---
+++ render.h
@@ -0,0 +1,9 @@
+#ifndef RENDER_H_CHECK
+#define RENDER_H_CHECK
+
+#include <iostream>
+#include "render.cpp"
+
+uint8_t 
+
+#endif
